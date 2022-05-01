741f2e36d02a5cf18419c32785db7882d4e7edf4
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-04-30 17:25:03 -0700

    Search bar

--- searchbar1.js
+++ searchbar1.js
@@ -1,14 +1,14 @@
 const charactersList = document.getElementById('tutorialList');
 const searchBar = document.getElementById('searchBar');
 let hpCharacters = [
-    {"name": "How to make a e to enter vehicle?", "image": '/media/tutorial1.png', "link": "/scriptingtutorials/e-to-enter-vehicle.html"},
-    {"name": "How to make a shift to sprint?", "image": '/media/tutorial2.png', "link": "/scriptingtutorials/shift-to-sprint.html"},
-    {"name": "How to make a day / night cycle?", "image": '/media/tutorial3.png', "link": "/scriptingtutorials/day-night-cycle.html"},
+    {"name": "How to make a e to enter vehicle?", "image": '/media/tutorial1.png', "link": "/scriptingtutorials/e-to-enter-vehicle.html", "skill": "(Simple)"},
+    {"name": "How to make a shift to sprint?", "image": '/media/tutorial2.png', "link": "/scriptingtutorials/shift-to-sprint.html", "skill": "(Simple)"},
+    {"name": "How to make a day / night cycle?", "image": '/media/tutorial3.png', "link": "/scriptingtutorials/day-night-cycle.html", "skill": "(Simple)"},
 ];
 
 
 searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
+    const searchString = (e.target.value.toLowerCase()).trim();
 
     const filteredCharacters = hpCharacters.filter((character) => {
         return (
@@ -26,6 +26,7 @@ const displayCharacters = (characters) => {
                 <a href="${character.link}">
                     <h2>${character.name}</h2>
                     <img id="selectionimage" src="${character.image}" />
+                    <h3>${character.skill}</h3>
                 </a>
             </div>
         `;
